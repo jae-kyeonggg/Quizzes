113db1082752236333b8ab7109489c0e6c5dabaf
경재한 <hanjaekyung56@gmail.com>
2023-07-11 08:29:54 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();   String grade;
+
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
+        }
+
+        System.out.println(grade);
+
+        sc.close();
+    }
+}
