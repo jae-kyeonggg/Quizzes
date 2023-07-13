aecc7c0943251a6938d8f7b5e62db16801ecae26
경재한 <hanjaekyung56@gmail.com>
2023-07-13 13:31:00 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 284 ms, Memory: 21408 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int a = sc.nextInt();
+            if (a == -1)
+                break;
+            sum += a;
+        }
+        System.out.println(sum);
+    }
+}
