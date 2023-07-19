7f4aff8c862b27f536d2c0bf87518aad52e049ed
경재한 <hanjaekyung56@gmail.com>
2023-07-19 15:27:08 +0900

    [Silver V] Title: 나이순 정렬, Time: 672 ms, Memory: 49636 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] ar = new String[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            ar[i][0] = st.nextToken();  ar[i][1] = st.nextToken();
+        }
+        Arrays.sort(ar, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(ar[i][0]).append(' ').append(ar[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
