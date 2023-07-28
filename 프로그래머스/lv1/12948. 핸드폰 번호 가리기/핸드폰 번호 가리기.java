81409c4049fb3fec28485923d172da98e8cf0bae
경재한 <hanjaekyung56@gmail.com>
2023-07-28 09:34:40 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.75 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        String star = "";
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            star += "*";
+        }
+        answer += star;
+        answer += phone_number.substring(phone_number.length() - 4);
+        
+        return answer;
+    }
+}
