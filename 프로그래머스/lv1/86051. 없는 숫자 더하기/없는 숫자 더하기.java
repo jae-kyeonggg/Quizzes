529e46791dcabfe269d809f5df7f35229201805a
경재한 <hanjaekyung56@gmail.com>
2023-07-28 09:34:49 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int total = 45;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            total -= numbers[i];
+        }
+        
+        return total;
+    }
+}
