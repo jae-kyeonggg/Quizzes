68f2af0f4cb9ae61bf6591d4164cd00ba99a909a
경재한 <hanjaekyung56@gmail.com>
2023-08-09 20:56:47 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int k = 0; k < name.length; k++) {
+                    if (photo[i][j].equals(name[k])) {
+                        answer[i] += (map.get(photo[i][j]) == null ? 0 : map.get(photo[i][j]));
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
