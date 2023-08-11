748b35d3ff834f6fc728e9ab6dac8bbdb81801da
경재한 <hanjaekyung56@gmail.com>
2023-08-11 15:48:29 +0900

    [level 1] Title: 소수 찾기, Time: 197.74 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static boolean isPrime(int n) {
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0) 
+                return false;
+        }
+        return true;
+    }
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i))
+                answer++;
+        }
+        return answer;
+    }
+}
