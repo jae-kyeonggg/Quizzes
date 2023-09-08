e1c18bef7aca07d8f356bb8d2de0ddfb6dd682ae
경재한 <hanjaekyung56@gmail.com>
2023-09-08 15:29:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 299.34 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Arrays.sort(participant);   Arrays.sort(completion);
+
+        for (int i = 0; i < participant.length; i++) {
+            if (i == completion.length) {
+                answer = participant[i];
+                break;
+            }
+            if (!completion[i].equals(participant[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+
+
