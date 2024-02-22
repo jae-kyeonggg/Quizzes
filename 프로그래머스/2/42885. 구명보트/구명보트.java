bf6b871a1b63d0a7ac2ef57e24c74a6d67b49385
경재한 <hanjaekyung56@gmail.com>
2024-02-23 01:02:32 +0900

    [level 2] Title: 구명보트, Time: 10.72 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                answer += 1;
+                index += 1;
+            } else {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
