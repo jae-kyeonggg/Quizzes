9d031a878088f29609e2eb65a44a50a44b0a13e8
경재한 <hanjaekyung56@gmail.com>
2024-02-22 22:12:17 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] ar = new int[commands[i][1] - commands[i][0] + 1];
+            for (int j = 0; j < commands[i][1] - commands[i][0] + 1; j++) {
+                ar[j] = array[j + commands[i][0] - 1];
+            }
+            Arrays.sort(ar);
+            answer[i] = ar[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
